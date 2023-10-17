907bda0be848770e4407909fdea240af326c3165
Tim Wiegand <1773971+timwiegand@users.noreply.github.com>
2023-10-17 13:07:39 +0100

    Tweaked text to test HMR

--- src/App.tsx
+++ src/App.tsx
@@ -16,13 +16,13 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React + TS</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit <code>src/App.tsx</code> and save to test HMR on The Candid Startup
         </p>
       </div>
       <p className="read-the-docs">
