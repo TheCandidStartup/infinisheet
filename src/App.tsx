855fe60e66c9ec48991d528d4e29cb567052f4f8
Tim Wiegand <1773971+timwiegand@users.noreply.github.com>
2023-10-26 18:19:27 +0100

    Hacked in VirtualScroller from https://github.com/dhilt/react-virtual-scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,35 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import VirtualScroller from './VirtualScroller'
+
+const SETTINGS = {
+  itemHeight: 20,
+  amount: 10,
+  tolerance: 5,
+  minIndex: -9999,
+  maxIndex: 100000,
+  startIndex: 1
+}
+
+const getData = (offset, limit) => {
+  const data = []
+  const start = Math.max(SETTINGS.minIndex, offset)
+  const end = Math.min(offset + limit - 1, SETTINGS.maxIndex)
+  console.log(`request [${offset}..${offset + limit - 1}] -> [${start}..${end}] items`)
+  if (start <= end) {
+    for (let i = start; i <= end; i++) {
+      data.push({ index: i, text: `item ${i}` })
+    }
+  }
+  return data
+}
+
+const rowTemplate = item => (
+  <div className="item" key={item.index}>
+    {item.text}
+  </div>
+)
 
 function App() {
   const [count, setCount] = useState(0)
@@ -21,6 +50,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <VirtualScroller className="viewport" get={getData} settings={SETTINGS} row={rowTemplate}/>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR on The Candid Startup
         </p>
