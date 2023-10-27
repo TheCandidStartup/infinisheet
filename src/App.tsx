b6917026b5621111cdc192b89940c893af645a98
Tim Wiegand <1773971+timwiegand@users.noreply.github.com>
2023-10-27 18:17:43 +0100

    Added the bare minimum type annotations to allow project to build. Can remove ts-ignore once VirtualScroller ported to TS.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+// @ts-ignore
 import VirtualScroller from './VirtualScroller'
 
 const SETTINGS = {
@@ -13,7 +11,7 @@ const SETTINGS = {
   startIndex: 1
 }
 
-const getData = (offset, limit) => {
+const getData = (offset: number, limit: number) => {
   const data = []
   const start = Math.max(SETTINGS.minIndex, offset)
   const end = Math.min(offset + limit - 1, SETTINGS.maxIndex)
@@ -26,38 +24,18 @@ const getData = (offset, limit) => {
   return data
 }
 
-const rowTemplate = item => (
+const rowTemplate = (item: any) => (
   <div className="item" key={item.index}>
     {item.text}
   </div>
 )
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React + TS</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <VirtualScroller className="viewport" get={getData} settings={SETTINGS} row={rowTemplate}/>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR on The Candid Startup
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
