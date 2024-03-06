4307c9ad408ee7366c26380649522fd658493eb5
Tim Wiegand <1773971+timwiegand@users.noreply.github.com>
2024-03-06 15:52:38 +0000

    Got basic custom hook unit test going

---
+++ src/useVirtualScroll.test.ts
@@ -0,0 +1,25 @@
+import { act, renderHook } from '@testing-library/react'
+import { useVirtualScroll } from './useVirtualScroll'
+
+describe('useVirtualScroll', () => {
+  it('should have initial value', () => {
+    const { result } = renderHook(() => useVirtualScroll())
+    const [{ scrollOffset, scrollDirection }] = result.current;
+    expect(scrollOffset).toBe(0);
+    expect(scrollDirection).toBe("forward");
+  })
+
+  it('should update offset and direction OnScroll', () => {
+    const { result } = renderHook(() => useVirtualScroll());
+    const [{}, onScrollExtent] = result.current;
+    
+    act(() => {
+      onScrollExtent(100, 1000, 50);
+    })
+    {
+      const [{ scrollOffset, scrollDirection }] = result.current;
+      expect(scrollOffset).toBe(50);
+      expect(scrollDirection).toBe("forward");
+    }
+  })
+})
