ad57f29ace8c9f7141bbbb54d6aed71b2ee0d337
Tim Wiegand <1773971+timwiegand@users.noreply.github.com>
2024-03-18 16:06:50 +0000

    Got scrolling working in unit test

--- src/VirtualList.test.tsx
+++ src/VirtualList.test.tsx
@@ -8,6 +8,27 @@ function throwErr(msg: string): never {
   throw msg;
 }
 
+// jsdom does not implement any layout. All layout related properties are defined but return 0. As they
+// are layout driven properties they're read only. Mock up just enough layout logic for the tests to work.
+// Need to use Object.defineProperty to override read only properties.
+function overrideProp(element: HTMLElement, prop: string, val: any) {
+  if (!(prop in element))
+    throw `Property ${prop} doesn't exist when trying to override`;
+
+  Object.defineProperty(element, prop, {
+    value: val,
+    writable: false
+  });
+}
+
+function updateLayout(innerDiv: HTMLElement, outerDiv: HTMLElement) {
+  const scrollHeight = parseInt(innerDiv.style.height);
+  overrideProp(outerDiv, "scrollHeight", scrollHeight);
+
+  const clientHeight = parseInt(outerDiv.style.height);
+  overrideProp(outerDiv, "clientHeight", clientHeight);
+}
+
 describe('Fixed Size VirtualList', () => {
   const Cell = ({ index, style }: { index: number, style: any }) => (
     <div className={ index == 0 ? "header" : "cell" } style={style}>
@@ -17,7 +38,7 @@ describe('Fixed Size VirtualList', () => {
     
   const mapping = useFixedSizeItemOffsetMapping(30);
   
-  it('should default to rendering the top of the list', () => {
+  it('should render and scroll', () => {
     render(
       <VirtualList
         height={240}
@@ -37,6 +58,7 @@ describe('Fixed Size VirtualList', () => {
     expect(item1).toHaveProperty("style.top", '30px')
     expect(item1).toHaveProperty("style.height", '30px')
 
+    // Overscan should render one item after visible window
     const item8 = screen.getByText('Item 8');
     expect(item8).toBeInTheDocument()
     expect(item8).toHaveProperty("style.top", '240px')
@@ -46,10 +68,34 @@ describe('Fixed Size VirtualList', () => {
 
     const innerDiv = header.parentElement || throwErr("No inner div");
     const outerDiv = innerDiv.parentElement || throwErr("No outer div");
+    updateLayout(innerDiv, outerDiv);
 
+    // Scroll down 4 items.
     {act(() => {
-      fireEvent.scroll(outerDiv, { target: { scrollTop: 100 }});
+      fireEvent.scroll(outerDiv, { target: { scrollTop: 120 }});
+      fireEvent(outerDiv, new UIEvent('scrollend'));
     })}
+    expect(screen.queryByText('header')).toBeNull()
     expect(screen.queryByText('Item 1')).toBeNull()
+    expect(screen.queryByText('Item 2')).toBeNull()
+
+    // Overscan should render one item before the start of the visible window
+    const item3 = screen.getByText('Item 3');
+    expect(item3).toBeInTheDocument()
+    expect(item3).toHaveProperty("style.top", '90px')
+    expect(item3).toHaveProperty("style.height", '30px')
+
+    // New items scrolled into view
+    const item9= screen.getByText('Item 9');
+    expect(item9).toBeInTheDocument()
+    expect(item9).toHaveProperty("style.top", '270px')
+    expect(item9).toHaveProperty("style.height", '30px')
+
+    const item12= screen.getByText('Item 12');
+    expect(item12).toBeInTheDocument()
+    expect(item12).toHaveProperty("style.top", '360px')
+    expect(item12).toHaveProperty("style.height", '30px')
+
+    expect(screen.queryByText('Item 13')).toBeNull()
   })
 })
