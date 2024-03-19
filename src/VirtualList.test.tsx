7779e5d3e95b29a3c07f60bbc8dc23b22e936202
Tim Wiegand <1773971+timwiegand@users.noreply.github.com>
2024-03-19 11:57:19 +0000

    Added another test case for variable sized items with useIsScrolling enabled and scrolling to end of list

--- src/VirtualList.test.tsx
+++ src/VirtualList.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen, fireEvent, act } from './test/wrapper'
 import { VirtualList } from './VirtualList'
 import { useFixedSizeItemOffsetMapping } from './useFixedSizeItemOffsetMapping';
+import { useVariableSizeItemOffsetMapping } from './useVariableSizeItemOffsetMapping';
 
 // Function wrapper around throw so that it can be used in an expression like
 //    const value = somethingThatIsValueOrNull || throwErr("Shouldn't be null")
@@ -97,5 +98,90 @@ describe('Fixed Size VirtualList', () => {
     expect(item12).toHaveProperty("style.height", '30px')
 
     expect(screen.queryByText('Item 13')).toBeNull()
+  })
+})
+
+describe('Variable Size VirtualList with useIsScrolling', () => {
+  const Cell = ({ index, isScrolling, style }: { index: number, isScrolling?: boolean, style: any }) => (
+    <div className={ index == 0 ? "header" : ( isScrolling ? "cellScroll" : "cell") } style={style}>
+      { (index == 0) ? "Header" : "Item " + index }
+    </div>
+  );
+    
+  // Header twice the size of a normal item
+  const mapping = useVariableSizeItemOffsetMapping(30, [60]);
+  
+  it('should render and scroll', () => {
+    render(
+      <VirtualList
+        height={240}
+        itemCount={100}
+        itemOffsetMapping={mapping}
+        useIsScrolling={true}
+        width={600}>
+        {Cell}
+      </VirtualList>
+    )
+    const header = screen.getByText('Header');
+    expect(header).toBeInTheDocument()
+    expect(header).toHaveProperty("style.top", '0px')
+    expect(header).toHaveProperty("style.height", '60px')
+
+    const item1 = screen.getByText('Item 1');
+    expect(item1).toBeInTheDocument()
+    expect(item1).toHaveProperty("style.top", '60px')
+    expect(item1).toHaveProperty("style.height", '30px')
+
+    // Should be one less item than fixed size case
+    const item7 = screen.getByText('Item 7');
+    expect(item7).toBeInTheDocument()
+    expect(item7).toHaveProperty("style.top", '240px')
+    expect(item7).toHaveProperty("style.height", '30px')
+
+    expect(screen.queryByText('Item 8')).toBeNull()
+
+    const innerDiv = header.parentElement || throwErr("No inner div");
+    const outerDiv = innerDiv.parentElement || throwErr("No outer div");
+    updateLayout(innerDiv, outerDiv);
+
+    // Check that trying to scroll past the end is handled sensibly
+    // Send scrollEnd separately so can check isScrolling property works
+    {act(() => {
+      fireEvent.scroll(outerDiv, { target: { scrollTop: 5000 }});
+    })}
+    expect(screen.queryByText('header')).toBeNull()
+    expect(screen.queryByText('Item 1')).toBeNull()
+    expect(screen.queryByText('Item 90')).toBeNull()
+
+    // Overscan should render one item before the start of the visible window
+    let item91 = screen.getByText('Item 91');
+    expect(item91).toBeInTheDocument()
+    expect(item91).toHaveProperty("style.top", '2760px')
+    expect(item91).toHaveProperty("style.height", '30px')
+    expect(item91).toHaveProperty("className", 'cellScroll')
+
+    // Last item in list should be visible
+    let item99 = screen.getByText('Item 99');
+    expect(item99).toBeInTheDocument()
+    expect(item99).toHaveProperty("style.top", '3000px')
+    expect(item99).toHaveProperty("style.height", '30px')
+
+    expect(screen.queryByText('Item 100')).toBeNull()
+
+    {act(() => {
+      fireEvent(outerDiv, new UIEvent('scrollend'));
+    })}
+
+    item91 = screen.getByText('Item 91');
+    expect(item91).toBeInTheDocument()
+    expect(item91).toHaveProperty("style.top", '2760px')
+    expect(item91).toHaveProperty("style.height", '30px')
+    expect(item91).toHaveProperty("className", 'cell')
+
+    item99 = screen.getByText('Item 99');
+    expect(item99).toBeInTheDocument()
+    expect(item99).toHaveProperty("style.top", '3000px')
+    expect(item99).toHaveProperty("style.height", '30px')
+    expect(item99).toHaveProperty("className", 'cell')
   })
 })
