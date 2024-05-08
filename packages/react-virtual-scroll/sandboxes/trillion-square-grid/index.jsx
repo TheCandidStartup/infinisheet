fde54b6cc6638fd4b8faa50e19a7aec77a13241c
Tim Wiegand <1773971+timwiegand@users.noreply.github.com>
2024-05-08 11:13:02 +0100

    Trying to put together a minimal standalone sample that could be loaded by a SandBox app.

---
+++ packages/react-virtual-scroll/sandboxes/trillion-square-grid/index.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { VirtualGrid, useVariableSizeItemOffsetMapping, useFixedSizeItemOffsetMapping } from '@candidstartup/react-virtual-scroll';
+
+import './styles.css';
+
+const Cell = ({ rowIndex, columnIndex, style }) => (
+  <div className={ rowIndex == 0 ? "header" : "cell" } style={style}>
+    { (rowIndex == 0) ? `${columnIndex}` : `${rowIndex}:${columnIndex}` }
+  </div>
+);
+
+function App() {
+  var mapping = useVariableSizeItemOffsetMapping(30, [50]);
+  var columnMapping = useFixedSizeItemOffsetMapping(280);
+  const grid = React.createRef();
+  const rowInput = React.createRef();
+  const columnInput = React.createRef();
+
+  function onChange() {
+    const row = rowInput.current?.valueAsNumber || 0;
+    const column = columnInput.current?.valueAsNumber || 0;
+    grid.current?.scrollToItem(row, column);
+  }
+
+  return (
+    <div className="app-container">
+      <label>
+        Row: 
+        <input
+          ref={rowInput}
+          type={"number"}
+          height={200}
+          onChange={onChange}
+        />
+      </label>
+      <label>
+        Col: 
+        <input
+          ref={columnInput}
+          type={"number"}
+          height={200}
+          onChange={onChange}
+        />
+      </label>
+
+      <VirtualGrid
+        ref={grid}
+        height={240}
+        rowCount={1000000000000}
+        rowOffsetMapping={mapping}
+        columnCount={1000000000000}
+        columnOffsetMapping={columnMapping}
+        width={600}>
+        {Cell}
+      </VirtualGrid>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
