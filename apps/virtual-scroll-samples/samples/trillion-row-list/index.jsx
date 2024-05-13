b6f140fcc32aa2c3cafab4af3a98aadca1bd8718
Tim Wiegand <1773971+timwiegand@users.noreply.github.com>
2024-05-13 18:14:44 +0100

    feat(virtual-scroll-samples): Restructured as a multi-page app with each sample as a separate page
    Includes current working TypeScript sample plus both Sandbox JSX samples. Configured Rollup so that all samples share a common chunk for React and any other vendor dependencies.

---
+++ apps/virtual-scroll-samples/samples/trillion-row-list/index.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { VirtualList, useVariableSizeItemOffsetMapping } from '@candidstartup/react-virtual-scroll';
+
+import './styles.css';
+
+const Row = ({ index, style }) => (
+  <div className={ index == 0 ? "header" : "cell" } style={style}>
+    { (index == 0) ? "Header" : "Item " + index }
+  </div>
+);
+
+function App() {
+  var mapping = useVariableSizeItemOffsetMapping(30, [50]);
+  const list = React.createRef();
+
+  return (
+    <div>
+      <label>
+        Item: 
+        <input
+          type={"number"}
+          height={200}
+          onChange={(event) => {
+            const value = parseInt(event.target.value);
+            list.current.scrollToItem(value)
+          }}
+        />
+      </label>
+
+      <VirtualList
+        ref={list}
+        height={240}
+        itemCount={1000000000000}
+        itemOffsetMapping={mapping}
+        width={600}>
+        {Row}
+      </VirtualList>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
--- apps/virtual-scroll-samples/vite.config.ts
+++ apps/virtual-scroll-samples/vite.config.ts
@@ -1,8 +1,8 @@
-/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import sourcemaps from '@gordonmleigh/rollup-plugin-sourcemaps';
+import sourcemaps from '@gordonmleigh/rollup-plugin-sourcemaps'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,7 +13,20 @@ export default defineConfig({
   build: {
     sourcemap: true,
     rollupOptions:  {
-      plugins: [sourcemaps()]
+      plugins: [sourcemaps()],
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        "list-and-grid": resolve(__dirname, 'samples/list-and-grid/index.html'),
+        "trillion-row-list": resolve(__dirname, 'samples/trillion-row-list/index.html'),
+        "trillion-square-grid": resolve(__dirname, 'samples/trillion-square-grid/index.html'),
+      },
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
     }
   }
 })
