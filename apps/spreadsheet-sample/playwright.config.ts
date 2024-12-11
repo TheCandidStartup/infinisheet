edd55f67f231f5e1081c9e2fbe6431b2ff8017c5
Tim Wiegand <1773971+timwiegand@users.noreply.github.com>
2024-12-11 10:17:31 +0000

    build: Run Playwright tests during Build CI workflow

--- apps/spreadsheet-sample/playwright.config.ts
+++ apps/spreadsheet-sample/playwright.config.ts
@@ -28,7 +28,8 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
 
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  // Concise 'dot' for CI, terminal 'list' when running locally
+  reporter: process.env.CI ? 'dot' : 'list',
 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
