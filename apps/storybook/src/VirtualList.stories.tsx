a51e0ca1a7ca103db942f39618d8b8e56d1726de
Tim Wiegand <1773971+timwiegand@users.noreply.github.com>
2025-02-04 18:42:34 +0000

    feat(storybook): Added "Interactive" args to VirtualList
    currentItem and currentOffset are read-only custom args that display scroll state reported by onScroll handler
    scrollToItem and scrollTo are custom args that call the corresponding VirtualListProxy methods

--- apps/storybook/src/VirtualList.stories.tsx
+++ apps/storybook/src/VirtualList.stories.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import type { Meta, StoryObj } from '@storybook/react';
 import { fn } from '@storybook/test';
+import { useArgs } from '@storybook/preview-api';
 
-import { VirtualList, VirtualListProps } from '@candidstartup/react-virtual-scroll';
+import { ScrollState, VirtualList, VirtualListProps, VirtualListProxy } from '@candidstartup/react-virtual-scroll';
 import { mappingVariableVertical, mappingFixedVertical, mappingFixedHorizontal, rewriteMapping } from './mapping';
 
 function rowClassName(rowIndex: number, isScrolling?: boolean): string {
@@ -29,15 +30,84 @@ const Column = ({ index, style, isScrolling }: { index: number, isScrolling?: bo
   </div>
 );
 
-const meta: Meta<VirtualListProps> = {
+type VirtualListPropsAndCustomArgs = VirtualListProps & { 
+  currentOffset?: number,
+  currentItem?: number,
+  scrollToOffset?: number,
+  scrollToItem?: number
+};
+
+const meta: Meta<VirtualListPropsAndCustomArgs> = {
   title: 'react-virtual-scroll/VirtualList',
   component: VirtualList,
-  render: ( {layout, children: _children, itemOffsetMapping, ...args}) => (
-    <VirtualList layout={layout} itemOffsetMapping={rewriteMapping(itemOffsetMapping, layout)}{...args}>
+  render: ( {layout, children: _children, currentItem, currentOffset, scrollToItem, scrollToOffset, onScroll, itemOffsetMapping, ...args}) => {
+    const [_, updateArgs] = useArgs();
+    const listProxy = React.useRef<VirtualListProxy>(null);
+
+    React.useEffect(() => { 
+      if (scrollToItem !== undefined) {
+        listProxy.current?.scrollToItem(scrollToItem)
+      }
+    }, [scrollToItem])
+
+    React.useEffect(() => { 
+      if (scrollToOffset !== undefined) {
+        listProxy.current?.scrollTo(scrollToOffset)
+      }
+    }, [scrollToOffset])
+
+    const currentMapping = rewriteMapping(itemOffsetMapping, layout);
+
+    function ScrollHandler(offset: number, newScrollState: ScrollState): void {
+      // Record event for actions tab
+      if (onScroll)
+        onScroll(offset, newScrollState);
+    
+      if (offset != currentOffset)
+        updateArgs({ currentOffset: offset });
+
+      const [item] = currentMapping.offsetToItem(offset);
+      if (item != currentItem)
+        updateArgs({ currentItem: item });
+    }
+
+    return <VirtualList ref={listProxy} layout={layout} itemOffsetMapping={currentMapping} onScroll={ScrollHandler} {...args}>
       {layout === 'horizontal' ? Column : Row}
     </VirtualList>
-  ),
+  },
   argTypes: {
+    currentItem: {
+      description: "Displays current item reported via `OnScroll` and `ItemOffsetMapping`",
+      table: {
+        category: "Interactive",
+        readonly: true
+      }
+    },
+    scrollToItem: {
+      description: "Scrolls to item using `VirtualListProxy.scrollToItem`",
+      table: {
+        category: "Interactive",
+      },
+      control: {
+        type: 'number'
+      }
+    },
+    currentOffset: {
+      description: "Displays current offset (in pixels) reported via `OnScroll`",
+      table: {
+        category: "Interactive",
+        readonly: true
+      }
+    },
+    scrollToOffset: {
+      description: "Scrolls to offset (in pixels) using `VirtualListProxy.scrollTo`",
+      table: {
+        category: "Interactive",
+      },
+      control: {
+        type: 'number'
+      }
+    },
     itemOffsetMapping: {
       options: ['Fixed', 'Variable'],
       mapping: {
@@ -46,8 +116,13 @@ const meta: Meta<VirtualListProps> = {
       }
     }
   },
-  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-  args: { onScroll: fn() },
+
+  args: { 
+    // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+    onScroll: fn(),
+    currentItem: 0,
+    currentOffset: 0
+  },
 };
 
 export default meta;
