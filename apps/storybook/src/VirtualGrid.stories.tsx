d9a0ccfd6449eab58948338bf09439906a79e1b9
Tim Wiegand <1773971+timwiegand@users.noreply.github.com>
2025-02-05 11:25:17 +0000

    feat(storybook): Added interactive controls to VirtualGrid

--- apps/storybook/src/VirtualGrid.stories.tsx
+++ apps/storybook/src/VirtualGrid.stories.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import type { Meta, StoryObj } from '@storybook/react';
 import { fn } from '@storybook/test';
+import { useArgs } from '@storybook/preview-api';
 
-import { VirtualGrid, VirtualGridProps, useFixedSizeItemOffsetMapping } from '@candidstartup/react-virtual-scroll';
+import { ScrollState, VirtualGrid, VirtualGridProps, VirtualGridProxy, useFixedSizeItemOffsetMapping } from '@candidstartup/react-virtual-scroll';
 import { mappingVariableVertical, mappingVariableHorizontal, mappingFixedVertical, mappingFixedHorizontal } from './mapping';
 
 const mappingWideHorizontal = useFixedSizeItemOffsetMapping(160);
@@ -19,15 +20,142 @@ const Cell = ({ rowIndex, columnIndex, isScrolling, style }: { rowIndex: number,
   </div>
 );
 
-const meta: Meta<VirtualGridProps> = {
+type VirtualGridPropsAndCustomArgs = VirtualGridProps & { 
+  currentRowOffset?: number,
+  currentColumnOffset?: number,
+  currentRow?: number,
+  currentColumn?: number,
+  scrollToRowOffset?: number,
+  scrollToColumnOffset?: number,
+  scrollToRow?: number,
+  scrollToColumn?: number
+};
+
+const meta: Meta<VirtualGridPropsAndCustomArgs> = {
   title: 'react-virtual-scroll/VirtualGrid',
   component: VirtualGrid,
-  render: ( {children: _children, ...args}) => (
-    <VirtualGrid {...args}>
+  render: ( { children: _children, rowOffsetMapping, columnOffsetMapping, onScroll,
+              currentRow, currentColumn, scrollToRow, scrollToColumn, 
+              currentRowOffset, currentColumnOffset, scrollToRowOffset, scrollToColumnOffset, 
+              ...args}) => {
+    const [_, updateArgs] = useArgs();
+    const gridProxy = React.useRef<VirtualGridProxy>(null);
+
+    React.useEffect(() => { 
+      if (scrollToRow !== undefined) {
+        gridProxy.current?.scrollToItem(scrollToRow, undefined);
+      }
+    }, [scrollToRow])
+
+    React.useEffect(() => { 
+      if (scrollToColumn !== undefined) {
+        gridProxy.current?.scrollToItem(undefined, scrollToColumn);
+      }
+    }, [scrollToColumn])
+
+    React.useEffect(() => { 
+      if (scrollToRowOffset !== undefined) {
+        gridProxy.current?.scrollTo(scrollToRowOffset, undefined)
+      }
+    }, [scrollToRowOffset])
+
+    React.useEffect(() => { 
+      if (scrollToColumnOffset !== undefined) {
+        gridProxy.current?.scrollTo(undefined, scrollToColumnOffset)
+      }
+    }, [scrollToColumnOffset])
+
+    function ScrollHandler(rowOffset: number, columnOffset: number, newRowScrollState: ScrollState, newColumnScrollState: ScrollState): void {
+      // Record event for actions tab
+      if (onScroll)
+        onScroll(rowOffset, columnOffset, newRowScrollState, newColumnScrollState);
+    
+      if (rowOffset != currentRowOffset)
+        updateArgs({ currentRowOffset: rowOffset });
+
+      if (columnOffset != currentColumnOffset)
+        updateArgs({ currentColumnOffset: columnOffset });
+
+      const [row] = rowOffsetMapping.offsetToItem(rowOffset);
+      if (row != currentRow)
+        updateArgs({ currentRow: row });
+
+      const [column] = columnOffsetMapping.offsetToItem(columnOffset);
+      if (column != currentColumn)
+        updateArgs({ currentColumn: column });
+    }
+
+    return <VirtualGrid ref={gridProxy} onScroll={ScrollHandler} 
+        rowOffsetMapping={rowOffsetMapping} columnOffsetMapping={columnOffsetMapping}
+        {...args}>
       {Cell}
     </VirtualGrid>
-  ),
+  },
   argTypes: {
+    currentRow: {
+      description: "Displays current row reported via `OnScroll` and `ItemOffsetMapping`",
+      table: {
+        category: "Interactive",
+        readonly: true
+      }
+    },
+    currentColumn: {
+      description: "Displays current column reported via `OnScroll` and `ItemOffsetMapping`",
+      table: {
+        category: "Interactive",
+        readonly: true
+      }
+    },
+    scrollToRow: {
+      description: "Scrolls to row using `VirtualGridProxy.scrollToItem`",
+      table: {
+        category: "Interactive",
+      },
+      control: {
+        type: 'number'
+      }
+    },
+    scrollToColumn: {
+      description: "Scrolls to column using `VirtualGridProxy.scrollToItem`",
+      table: {
+        category: "Interactive",
+      },
+      control: {
+        type: 'number'
+      }
+    },
+    currentRowOffset: {
+      description: "Displays current row offset (in pixels) reported via `OnScroll`",
+      table: {
+        category: "Interactive",
+        readonly: true
+      }
+    },
+    currentColumnOffset: {
+      description: "Displays current column offset (in pixels) reported via `OnScroll`",
+      table: {
+        category: "Interactive",
+        readonly: true
+      }
+    },
+    scrollToRowOffset: {
+      description: "Scrolls to row offset (in pixels) using `VirtualGridProxy.scrollTo`",
+      table: {
+        category: "Interactive",
+      },
+      control: {
+        type: 'number'
+      }
+    },
+    scrollToColumnOffset: {
+      description: "Scrolls to column offset (in pixels) using `VirtualGridProxy.scrollTo`",
+      table: {
+        category: "Interactive",
+      },
+      control: {
+        type: 'number'
+      }
+    },
     rowOffsetMapping: {
       options: ['Fixed', 'Variable'],
       mapping: {
@@ -44,8 +172,13 @@ const meta: Meta<VirtualGridProps> = {
       }
     },
   },
-  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-  args: { onScroll: fn() },
+  args: { 
+    onScroll: fn(),
+    currentRow: 0,
+    currentColumn: 0,
+    currentColumnOffset: 0,
+    currentRowOffset: 0
+  },
 };
 
 export default meta;
