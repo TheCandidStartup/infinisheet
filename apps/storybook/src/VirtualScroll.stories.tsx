15c27f1f5ac61ea54a01caa8acdb92fe9316ecdb
Tim Wiegand <1773971+timwiegand@users.noreply.github.com>
2025-02-05 10:56:04 +0000

    feat(storybook): Added interactive ScrollTo controls to VirtualScroll

--- apps/storybook/src/VirtualScroll.stories.tsx
+++ apps/storybook/src/VirtualScroll.stories.tsx
@@ -1,7 +1,8 @@
+import React from "react";
 import type { Meta, StoryObj } from '@storybook/react';
 import { fn } from '@storybook/test';
 
-import { VirtualScroll, VirtualScrollProps } from '@candidstartup/react-virtual-scroll';
+import { VirtualScroll, VirtualScrollProps, VirtualScrollProxy } from '@candidstartup/react-virtual-scroll';
 
 // Auto-generated code (based on rendered JSX) can't show how child render function is used
 const displayCode = `
@@ -15,11 +16,30 @@ const displayCode = `
   )}
 </VirtualScroll>`
 
-const meta: Meta<VirtualScrollProps> = {
+type VirtualScrollPropsAndCustomArgs = VirtualScrollProps & { 
+  scrollToVerticalOffset?: number,
+  scrollToHorizontalOffset?: number
+};
+
+const meta: Meta<VirtualScrollPropsAndCustomArgs> = {
   title: 'react-virtual-scroll/VirtualScroll',
   component: VirtualScroll,
-  render: ( {...args} ) => (
-    <VirtualScroll {...args}>
+  render: ( {scrollToVerticalOffset, scrollToHorizontalOffset, ...args} ) => {
+    const proxy = React.useRef<VirtualScrollProxy>(null);
+
+    React.useEffect(() => { 
+      if (scrollToVerticalOffset !== undefined) {
+        proxy.current?.scrollTo(scrollToVerticalOffset, undefined);
+      }
+    }, [scrollToVerticalOffset])
+
+    React.useEffect(() => { 
+      if (scrollToHorizontalOffset !== undefined) {
+        proxy.current?.scrollTo(undefined, scrollToHorizontalOffset);
+      }
+    }, [scrollToHorizontalOffset])
+
+    return <VirtualScroll ref={proxy} {...args}>
       {({ isScrolling, verticalOffset, horizontalOffset }) => (
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', padding: 20 }}>
           isScrolling: {isScrolling ? 'true' : 'false'} <br/>
@@ -28,8 +48,28 @@ const meta: Meta<VirtualScrollProps> = {
         </div>
       )}
     </VirtualScroll>
-  ),
+  },
 
+  argTypes: {
+    scrollToVerticalOffset: {
+      description: "Scrolls to vertical offset (in pixels) using `VirtualScrollProxy.scrollTo`",
+      table: {
+        category: "Interactive",
+      },
+      control: {
+        type: 'number'
+      }
+    },
+    scrollToHorizontalOffset: {
+      description: "Scrolls to horizontal offset (in pixels) using `VirtualScrollProxy.scrollTo`",
+      table: {
+        category: "Interactive",
+      },
+      control: {
+        type: 'number'
+      }
+    },
+  },
   args: { onScroll: fn() },
 };
 
