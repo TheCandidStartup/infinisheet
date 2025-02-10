eea549a59450709e04d75bfc3cdc9dcc688f5ad5
Tim Wiegand <1773971+timwiegand@users.noreply.github.com>
2025-02-10 18:17:07 +0000

    feat(storybook): Replaced TSDoc link tags with link target using code markup
    Links replaced in component, arg and param descriptions
    Not that param descriptions are simple text not Markdown so code style comes through as literal backtick quotes
    Using string.replaceAll which requires TypeScript compiler lib and target at least at ES2021

--- apps/storybook/src/extract.ts
+++ apps/storybook/src/extract.ts
@@ -2,12 +2,21 @@ import type { StrictArgTypes, SBType } from 'storybook/internal/types';
 import type { ArgTypesExtractor, Component } from 'storybook/internal/docs-tools';
 import { extractComponentProps, extractComponentDescription as baseExtractComponentDescription} from 'storybook/internal/docs-tools';
 
+// Strip out {@link type} tags. Most appropriate default is to format type as code. Could put in actual markdown links in future.
+function replaceLinks(description: string): string {
+  return description.replaceAll(/{@link\s+([^}]+)}/g, (_substring, p1) => {
+    const value = p1 as string;
+    return "`" + value + "`";
+  })
+}
+
 export const extractComponentDescription = (component: Component): string => { 
   const description = baseExtractComponentDescription(component);
 
-  const noGroup = description.replace(/\n\s*@group\s+([^\n`]+)(\n|$)/, "");
+  const noGroup = description.replace(/\n\s*@group\s+([^\n]+)(\n|$)/, "");
+  const fixedLinks = replaceLinks(noGroup);
 
-  return noGroup;
+  return fixedLinks;
 };
 
 export const extractArgTypes: ArgTypesExtractor = (component) => {
@@ -16,13 +25,17 @@ export const extractArgTypes: ArgTypesExtractor = (component) => {
     return null;
 
   return props.reduce((acc: StrictArgTypes, prop) => {
-    let { type, defaultValue } = prop.propDef;
-    const { name, description, jsDocTags, required } = prop.propDef;
+    let { type, defaultValue, description } = prop.propDef;
+    const { name, jsDocTags, required } = prop.propDef;
     const sbType = prop.propDef.sbType as SBType; // Annoyingly sbType is typed as any rather than SBType
 
+    // Default uses "union" as the type description for these cases. Provide something useful instead.
     if (sbType.name === 'enum' || sbType.name === 'union')
       type = { summary: sbType.name, detail: sbType.raw };
 
+    // Default removes and ignores @defaultValue tags. Look them up in the original docgenInfo
+    // and use them. There's limited space in "Default" column so use summary and detail if value
+    // too large.
     const result = prop.docgenInfo.description.match(/\n\s*@defaultValue\s+`*([^\n`]+)`*(\n|$)/);
     if (result) {
       const value = result[1];
@@ -32,6 +45,15 @@ export const extractArgTypes: ArgTypesExtractor = (component) => {
         defaultValue = { summary: value }
       }
     }
+
+    if (description)
+      description = replaceLinks(description);
+
+    // Note that param descriptions are simple text rather than Markdown
+    jsDocTags?.params?.forEach((param) => {
+      if (param.description)
+        param.description = replaceLinks(param.description);
+    })
 
     acc[name] = { name, description, 
       type: { required, ...sbType },
