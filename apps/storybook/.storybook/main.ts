08bfc9480387bc7a31181951b9abfa17cc1010cc
Tim Wiegand <1773971+timwiegand@users.noreply.github.com>
2025-02-13 09:36:41 +0000

    build(storybook): Building both regular and --test version
    Regular version built against source code for full set of autodocs and controls
    Test version build against packages for proper component testing. No docs, partial controls.

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -23,17 +23,21 @@ const config: StorybookConfig = {
     name: getAbsolutePath("@storybook/react-vite"),
     options: {},
   },
-  async viteFinal(config, { configType }) { 
+  async viteFinal(config, options) { 
     const { mergeConfig } = await import("vite");
 
-    // Production build doesn't use tsconfigPaths plugin
+    // Production build for testing doesn't use tsconfigPaths plugin
     // to ensure that it builds against dependent packages
     // from the monorepo rather than pulling in src directly.
     // Want to make sure that I'm eating my own dog food.
-    if (configType === 'PRODUCTION')
+    // Doing this disables Autodocs but that's alright because docs
+    // are excluded from --test builds.
+    if (options.configType === 'PRODUCTION' && options.test) {
+      console.log("Production build for testing - importing from monorepo built packages");
       return config;
+    }
 
-    // Add tsconfigPaths plugin for development build for
+    // Add tsconfigPaths plugin for other builds for Autodocs support and
     // nice development experience where I can edit src files
     // in dependent packages and have storybook immediately update.
     return mergeConfig(config, {
