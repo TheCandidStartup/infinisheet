02460f3683d8bcf102b01e9b2e6a66e717ea87e4
Tim Wiegand <1773971+timwiegand@users.noreply.github.com>
2026-08-13 16:31:15 +0100

    build(infinisheet): Replaced plugin-react-swc with the default plugin-react
    swc no longer has a speed advantage

--- packages/react-virtual-scroll/vite.config.ts
+++ packages/react-virtual-scroll/vite.config.ts
@@ -2,7 +2,7 @@
 import { defineConfig } from 'vite'
 import { mergeConfig } from 'vitest/config'
 import configShared from '../../shared/vitest.config.js'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default mergeConfig(
